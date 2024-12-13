ae59c45ec0716d3770a78809104385932d72303b
Jeremy Lindsay <jeremy@jeremylindsay.com>
2024-12-13 21:59:35 +1100

    modifid grid generator to use GridFactory instead

--- experiments/grid.py
+++ experiments/grid.py
@@ -1,15 +1,22 @@
 import numpy as np
+import torch
 
-class Grid():
-    def __init__(self, num_rows, num_cols, default_val=0):
+class GridFactory():
+    def __init__(
+        self,
+        num_rows,
+        num_cols,
+    ):
         self.num_rows = num_rows
         self.num_cols = num_cols
-        self.grid = np.full((self.num_rows, self.num_cols), default_val)
+    
+    def generate_empty(self, default_val=0):
+        return torch.empty(self.num_rows, self.num_cols).fill_(default_val)
 
-    def print(self):
-        print()
-        for row in self.grid: print(row)
-        print()
+    def generate_random(self, scale=1, offset=0):
+        return torch.rand(self.num_rows, self.num_cols) * scale + offset
 
-    def random(self, values=list(range(10))):
-        self.grid = np.random.choice(values, size=(self.num_rows, self.num_cols))
+class GridBatch():
+    def generate_batch(grid_generator, batch_size):
+        grids = [grid_generator().unsqueeze(0) for _ in range(batch_size)]
+        return torch.stack(grids)  # shape (batch_size, 1, num_rows, num_cols)
