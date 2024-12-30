a280f49af3ddbe90937930a98501ff6566412d20
Jeremy Lindsay <jeremy@jeremylindsay.com>
2024-12-30 12:34:03 +1100

    removed stable baselines references from grid autoencoder

--- src/models/grid_autoencoder.py
+++ src/models/grid_autoencoder.py
@@ -1,6 +1,4 @@
-import torch
 import torch.nn as nn
-from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class GridAutoencoder(nn.Module):
     def __init__(self):
@@ -28,38 +26,4 @@ class GridAutoencoder(nn.Module):
         latent = self.encoder(x)
         reconstructed = self.decoder(latent)
         return reconstructed
-
-class GridEncoderStableBaselines(BaseFeaturesExtractor):
-    def __init__(
-            self,
-            observation_space,
-            pretrained_model,
-            features_dim=128
-    ):
-        super(GridEncoderStableBaselines, self).__init__(
-            observation_space,
-            features_dim
-        )
-
-        self.pretrained_model = pretrained_model
-        for param in pretrained_model.parameters():
-            param.requires_grad = False
-
-        self.fc = nn.Linear(self._get_output_dim(observation_space), features_dim)
-
-    def forward(self, observations):
-        if observations.shape == (1, 4):
-            observations = observations.view(1, 1, 2, 2)
-        elif observations.shape == (64, 4):
-            observations = observations.view(64, 1, 2, 2)
-
-        with torch.no_grad():
-            features = self.pretrained_model.encoder(observations)
-        return self.fc(features)
-
-    def _get_output_dim(self, observation_space):
-        dummy_input = torch.zeros(1, *observation_space.shape)
-        with torch.no_grad():
-            features = self.pretrained_model.encoder(dummy_input.unsqueeze(0).unsqueeze(0))
-        return features.shape[1]
 
